e91d275b3c3ba9a827f4a380f657eb4df6ca6316
Arefa313 <arefa.noori.1380@gmail.com>
2024-09-28 00:52:39 +0430

    fix the root path

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.scss';
 import Show from './pages/Show';
 import Home from './pages/Home';
@@ -14,6 +14,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/Cryptopanel/" element={<Home />} />
+          <Route path="*" element={<Navigate to="/Cryptopanel/" />} />
           <Route path="/Cryptopanel/:id" element={<Show />} />
         </Routes>
       </main>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer = () => (
-  <p className="copyright">&copy; 2023 Crypto Panel | Developed by Abdulali Noori</p>
+  <p className="copyright">&copy; 2024 Crypto Panel | Developed by Arefa</p>
 );
 
 export default Footer;
